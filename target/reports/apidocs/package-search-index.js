d92f32d9adca2c9e9ba57a091b1ccbc223b59519
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 10:50:53 +0800

    chore(build): 添加.gitignore文件并清理构建产物
    - 添加标准Java项目.gitignore配置文件
    - 移除target目录下的API文档生成文件
    - 清理Maven构建产生的测试报告文件
    - 删除编译后的class文件和jar包
    - 排除IDE相关配置文件和临时文件

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.faceplusplus.spring.boot"},{"l":"com.faceplusplus.spring.boot.req"},{"l":"com.faceplusplus.spring.boot.resp"}];updateSearchResults();
